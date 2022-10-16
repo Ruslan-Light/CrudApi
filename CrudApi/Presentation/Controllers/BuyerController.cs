6bf076266237748ace8a64d92e2f0728c5c4bd4d
Ruslan-Light <73093065+Ruslan-Light@users.noreply.github.com>
2022-10-16 22:39:10 +0300

    Presentation part-1

---
+++ CrudApi/Presentation/Controllers/BuyerController.cs
@@ -0,0 +1,50 @@
+﻿using Application.UseCases.BuyerCases.AddBuyer;
+using Application.UseCases.BuyerCases.EditBuyer;
+using Application.UseCases.BuyerCases.GetBuyer;
+using Application.UseCases.BuyerCases.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    public class BuyerController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<BuyerVm>> GetBuyerById([FromQuery] GetBuyerQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> EditBuyer([FromBody] EditBuyerCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> AddBuyer([FromBody] AddBuyerCommand command)
+        {
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteBuyer(Guid id)
+        {
+            return NoContent();
+        }
+    }
+}
