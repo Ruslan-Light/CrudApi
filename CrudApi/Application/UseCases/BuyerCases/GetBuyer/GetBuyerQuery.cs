2d242532a0bdc799a5fd05c445a10805b55dd1ad
Ruslan-Light <73093065+Ruslan-Light@users.noreply.github.com>
2022-10-16 23:31:03 +0300

    New Application

---
+++ CrudApi/Application/UseCases/BuyerCases/GetBuyer/GetBuyerQuery.cs
@@ -0,0 +1,50 @@
+﻿using Application.EntityDbContext;
+using Application.Exceptions;
+using Application.UseCases.BuyerCases.ViewModels;
+using AutoMapper;
+using Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.BuyerCases.GetBuyer
+{
+    public class GetBuyerQuery : Profile, IRequest<BuyerVm>
+    {
+        public Guid Id { get; set; }
+
+        public GetBuyerQuery()
+        {
+            CreateMap<Buyer, BuyerVm>();
+        }
+
+        private class Handler : IRequestHandler<GetBuyerQuery, BuyerVm>
+        {
+            private readonly IApiContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(
+                IApiContext context,
+                IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<BuyerVm> Handle(GetBuyerQuery request, CancellationToken cancellationToken)
+            {
+                var user = _context.Buyers
+                    .AsNoTracking()
+                    .FirstOrDefault(b => b.Id == request.Id)
+                        ?? throw new NotFoundException("Пользователь не найден в системе");
+
+                var buyer = _mapper.Map<BuyerVm>(user);
+
+                return buyer;
+            }
+        }
+    }
+}
